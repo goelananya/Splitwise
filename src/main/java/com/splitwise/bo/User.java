0c39714671398c9e9b054d1019bdeae383ceddfc
Ananya <ananya.goel10@gmail.com>
2020-06-04 10:21:58 +0530

    User builder added; balanceBookId added to user entity

--- src/main/java/com/splitwise/bo/User.java
+++ src/main/java/com/splitwise/bo/User.java
@@ -1,8 +1,18 @@
 package com.splitwise.bo;
 
+import com.splitwise.builder.UserBuilder;
+
 public class User {
     private String username;
     private String email;
     private String phoneNumber;
+    private String balanceBookId;
+
+    public User(UserBuilder userBuilder) {
+        this.username = userBuilder.getUserName();
+        this.email = userBuilder.getEmail();
+        this.phoneNumber = userBuilder.getPhoneNumber();
+        this.balanceBookId = userBuilder.getBalanceBookId();
+    }
 
 }
---
+++ src/main/java/com/splitwise/builder/UserBuilder.java
@@ -0,0 +1,50 @@
+package com.splitwise.builder;
+
+import com.splitwise.bo.User;
+
+public class UserBuilder {
+    private String userName;
+    private String email;
+    private String phoneNumber;
+    private String balanceBookId;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public UserBuilder setUserName(String userName) {
+        this.userName = userName;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public UserBuilder setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public UserBuilder setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        return this;
+    }
+
+    public String getBalanceBookId() {
+        return balanceBookId;
+    }
+
+    public UserBuilder setBalanceBookId(String balanceBookId) {
+        this.balanceBookId = balanceBookId;
+        return this;
+    }
+
+    public User build() {
+        return new User(this);
+    }
+}
